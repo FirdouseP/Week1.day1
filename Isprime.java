88e27057534eb460f8d30a588bcda8494b501492
FirdouseP <108988658+FirdouseP@users.noreply.github.com>
2022-07-09 23:58:00 +0530

    Add files via upload

---
+++ Isprime.java
@@ -0,0 +1,19 @@
+package firstclass1;
+
+public class Isprime {
+
+	public static void main(String[] args) {
+		int n=53;
+		boolean prime=true;
+		for (int i = 2; i < n; i++) {
+		
+			if(n%i==0)
+			{
+				prime=false;
+				break;
+			}
+		}
+System.out.println(prime);
+	}
+
+}
